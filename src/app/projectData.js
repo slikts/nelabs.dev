2faab72e4516f11ba91e8480d8faba6d2344fb3a
slikts <dabas@untu.ms>
2020-04-03 10:05:39 +0000

    use local assets

--- src/app/projectData.js
+++ src/app/projectData.js
@@ -15,21 +15,21 @@ export default [
     name: "Queueable",
     url: "https://github.com/slikts/queueable#readme",
     img:
-      "https://raw.githubusercontent.com/slikts/queueable/master/logo.svg?sanitize=true",
+      "./assets/queueable.svg",
     desc: "CSP channels implemented using ES2018 async iteration"
   },
   {
     name: "Concurrency Glossary",
     url: "https://slikts.github.io/concurrency-glossary/",
     img:
-      "https://raw.githubusercontent.com/slikts/concurrency-glossary/master/logo.svg?sanitize=true",
+      "./assets/glossary.svg",
     desc: "Informal overview of terms used in concurrent computing"
   },
   {
     name: "Tuplerone",
     url: "https://github.com/slikts/tuplerone#readme",
     img:
-      "https://raw.githubusercontent.com/slikts/tuplerone/master/logo.svg?sanitize=true",
+      "./assets/tuplerone.svg",
     desc: "<code>WeakMap</code>-based tuples for JavaScript"
   },
   // {
