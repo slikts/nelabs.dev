7de3252af4a58a32c5d5474957babf70c3860c26
slikts <slikts@users.noreply.github.com>
2020-05-15 17:26:30 +0000

    deployed via Github Pages Deploy Action 🚀 for dce66f9c7a8b1825f7df280f93d49bb7347137ee

---
+++ precache-manifest.134bfcfd8004c29fe1ee71567f8945e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fe2b3540092a0dfe7ad91154b6353a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da89692dbcd97ee3ab41",
+    "url": "/static/css/main.4505a0a7.chunk.css"
+  },
+  {
+    "revision": "22fdaf42d3d8bb25eb8e",
+    "url": "/static/js/2.4b8dd857.chunk.js"
+  },
+  {
+    "revision": "4b4de5daed8b26db3ad36c6729bef15a",
+    "url": "/static/js/2.4b8dd857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da89692dbcd97ee3ab41",
+    "url": "/static/js/main.4e90eab6.chunk.js"
+  },
+  {
+    "revision": "cc22ab30add4822e9c62",
+    "url": "/static/js/runtime-main.df4a01b4.js"
+  },
+  {
+    "revision": "0255bf2cd6e42c2fdbe2e189e418929f",
+    "url": "/static/media/github-icon.0255bf2c.svg"
+  },
+  {
+    "revision": "c4f438fa6064cce71e3a97aae3d9684a",
+    "url": "/static/media/twitter-icon.c4f438fa.svg"
+  }
+]);
