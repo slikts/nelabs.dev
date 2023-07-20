a629cda9ad0d984d7015a46c02bb7ac58ac574b2
slikts <dabas@untu.ms>
2023-07-20 18:56:58 +0300

    upd

--- src/app/articleData.js
+++ src/app/articleData.js
@@ -5,39 +5,45 @@ export default [
   //   url: ""
   // },
   {
-    title: "Deno is probably not worth it right now",
-    date: "2020-05-15",
-    url: "https://dev.to/slikts/deno-is-probably-not-worth-it-right-now-1gg"
+    title:
+      'Extending native JS prototypes is not such a crazy idea with symbols',
+    date: '2023-07-20',
+    url: 'https://dev.to/slikts/extending-native-js-prototypes-is-not-such-a-crazy-idea-with-symbols-2h8l',
   },
   {
-    title: "React context vs Redux in 2020",
-    date: "2020-02-25",
-    url: "https://gist.github.com/slikts/57ff1acdb6f5b2ea075b701d1daf896d"
+    title: 'Deno is probably not worth it right now',
+    date: '2020-05-15',
+    url: 'https://dev.to/slikts/deno-is-probably-not-worth-it-right-now-1gg',
   },
   {
-    title: "Advanced memoization and effects in React",
-    date: "2020-02-24",
-    url: "https://gist.github.com/slikts/fd3768de1493419ed9506002b452fcdc"
+    title: 'React context vs Redux in 2020',
+    date: '2020-02-25',
+    url: 'https://gist.github.com/slikts/57ff1acdb6f5b2ea075b701d1daf896d',
+  },
+  {
+    title: 'Advanced memoization and effects in React',
+    date: '2020-02-24',
+    url: 'https://gist.github.com/slikts/fd3768de1493419ed9506002b452fcdc',
   },
   {
     title:
-      "Why using the <code>children</code> prop makes <code>React.memo()</code> not work",
-    date: "2020-02-22",
-    url: "https://gist.github.com/slikts/e224b924612d53c1b61f359cfb962c06"
+      'Why using the <code>children</code> prop makes <code>React.memo()</code> not work',
+    date: '2020-02-22',
+    url: 'https://gist.github.com/slikts/e224b924612d53c1b61f359cfb962c06',
   },
   {
-    title: "A case for using <code>void</code> in modern JavaScript",
-    date: "2019-12-09",
-    url: "https://gist.github.com/slikts/dee3702357765dda3d484d8888d3029e"
+    title: 'A case for using <code>void</code> in modern JavaScript',
+    date: '2019-12-09',
+    url: 'https://gist.github.com/slikts/dee3702357765dda3d484d8888d3029e',
   },
   {
-    title: "Appreciating the elegant simplicity of Immer",
-    date: "2019-10-17",
-    url: "https://gist.github.com/slikts/a975de5507e687a3718ad55c6745e9f5"
+    title: 'Appreciating the elegant simplicity of Immer',
+    date: '2019-10-17',
+    url: 'https://gist.github.com/slikts/a975de5507e687a3718ad55c6745e9f5',
   },
   {
-    title: "Linuxifying Windows for development",
-    date: "2019-07-25",
-    url: "https://gist.github.com/slikts/63abbeb63b72b3f515c70258bfc19a44"
-  }
-];
+    title: 'Linuxifying Windows for development',
+    date: '2019-07-25',
+    url: 'https://gist.github.com/slikts/63abbeb63b72b3f515c70258bfc19a44',
+  },
+]
